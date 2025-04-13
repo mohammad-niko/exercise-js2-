08c6c46a38a75cc32d747b9ea39054dcdb5dbf0c
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-04-13 19:50:26 +0330

    EXERCISE

--- exercise-js9-file.js
+++ exercise-js9-file.js
@@ -159,15 +159,119 @@
 // // ------------------------------------------------------------------js-10------------------------------------------------------------------
 // // ------------------------------------------------------------------
 // // ------------------------------------------------------------------
-const key = "name";
-const value = "mmad";
-const person = {
-  age: {
-    "last year": 20,
-    "This year": 21,
+// const key = "name";
+// const value = "mmad";
+// const person = {
+//   age: {
+//     "last year": 20,
+//     "This year": 21,
+//   },
+//   city: "Tabriz",
+// };
+// person[key] = value
+// console.log(person);
+// console.log(person[key]);
+// // ------------------------------------------------------------------
+
+
+
+
+// const fieldName = prompt("Enter the field name:");
+// const fieldValue = prompt("Enter the field value:");
+
+// const person = {};
+// person[fieldName] = fieldValue;;
+
+// console.log(person);
+
+
+
+// const keys = ["name" , "age" , "city"];
+// const person = {};
+
+// for(let i = 0 ; i<keys.length ; i++){
+//   person[keys[i]]= "unknown"
+// };
+// console.log(person);
+// // ------------------------------------------------------------------
+
+// const person = {
+//   name : "Ali",
+//   age: {
+//     "last year": 20,
+//     "This year": 21,
+//   },
+//   city: "Tabriz",
+//   setName (newName){
+//      this.name = newName
+//     }
+// };
+
+// // ------------------------------------------------------------------
+
+// person.setName("mmad");
+
+// console.log(person);
+
+// const user = {
+//   name: "Reza",
+//   age: 30,
+//   setName(newName) {
+//     this.name = newName;
+//   },
+//   growOlder() {
+//     this.age += 1;
+//   },
+//   printInfo() { 
+//     console.log(`Name: ${this.name}, Age: ${this.age}`);
+//   }
+// };user.setName("mmad");
+//   user.setName("ali");
+//   console.log(user);
+//   console.log(user);
+// // ------------------------------------------------------------------
+// const key ="name" ;
+// const person = {}
+
+
+// person[key] =["mmad","ali","reza"]
+
+// console.log(person["name"]);
+const students = {
+  student1: {
+    name: "Ali",
+    grades: {
+      math: 18,
+      english: 17,
+      science: 19
+    }
   },
-  city: "Tabriz",
+  student2: {
+    name: "Sara",
+    grades: {
+      math: 20,
+      english: 19,
+      science: 18
+    }
+  }
 };
-person[key] = value
-console.log(person);
-console.log(person[key]);
+let avreage1 = 0;
+let avreage2 = 0;
+let limietd = 0;
+let avreages = [avreage1 , avreage2]
+for (let key in students){
+  limietd++;
+for(let per in students[key].grades){
+   console.log(students.student1.grades[per]);
+   if(limietd > 2){
+     avreage1 = avreage1 += students.student1.grades[per] 
+   }else if(limietd === 2){
+      avreage2 =avreage2 += students.student1.grades[per] 
+   };
+}
+for(let i = 0 ; i < avreages.length ; i++){
+
+  console.log(`${key}'s average grade is ${avreages[(i / 3)]}`);
+}
+}
+console.log(avreage1 / 3);
