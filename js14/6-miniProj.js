d8d371f8bc5c478c4f98e154749d2a1e4b740bae
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-05-03 09:43:57 +0330

    js-14 Q

--- js14/6-miniProj.js
+++ js14/6-miniProj.js
@@ -7,3 +7,35 @@
 //todo2: Completed challenge has background green
 //todo3: Ongoing challenge has background yellow
 //todo4: Coming challenges have background red
+
+const h1 = document.querySelector("h1");
+
+const h1Span = document.createElement("span");
+
+h1Span.innerText = "2020";
+
+h1.innerText = "Asabeneh Yetayeh challenges in ";
+h1.appendChild(h1Span);
+
+console.log(h1);
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+setInterval(() => {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  h1Span.style.color = rndCol;
+}, 1000);
+
+const listItem = document.querySelectorAll("ul li");
+
+listItem.forEach((li) => {
+  if (li.innerText.includes("Done")) {
+    li.style.backgroundColor = "green";
+  } else if (li.innerText.includes("Ongoing")) {
+    li.style.backgroundColor = "yellow";
+  } else if (li.innerText.includes("Coming")) {
+    li.style.backgroundColor = "red";
+  }
+});
