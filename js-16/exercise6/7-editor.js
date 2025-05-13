8f4afd8db92b5ef90cf9316c3357f14b60f9b229
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-05-13 14:30:48 +0330

    js-16 Q

--- js-16/exercise6/7-editor.js
+++ js-16/exercise6/7-editor.js
@@ -6,3 +6,32 @@ Look at editor.png, you should create a text editor similar to that.
 - The third button bolds the text when clicked.
 - The fourth button italicizes the text when clicked.
 */
+const parentOfButtoms = document.querySelector(".circles-par");
+const textTarea = document.querySelector("#texttarea");
+parentOfButtoms.addEventListener("click", (e) => {
+  if (e.target.classList.contains("first-cir")) {
+    textTarea.value = textTarea.value.slice(0, -1);
+  }
+  if (e.target.classList.contains("second-cir")) {
+    textTarea.value = "";
+  }
+
+  if (e.target.classList.contains("third-cir")) {
+    console.log(e.target.classList.toggle("active"));
+    console.log(e.target.classList);
+    if (e.target.classList.contains("active")) {
+      textTarea.style.fontWeight = "bold";
+    } else {
+      textTarea.style.fontWeight = "normal";
+    }
+  }
+
+  if (e.target.classList.contains("fourth-cir")) {
+    e.target.classList.toggle("active");
+    if (e.target.classList.contains("active")) {
+      textTarea.style.fontStyle = "italic";
+    } else {
+      textTarea.style.fontStyle = "normal";
+    }
+  }
+});
