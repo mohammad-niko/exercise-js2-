41780616c5ddc30d32d15efe9419a395160fa6ed
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-05-25 16:54:10 +0330

    exercise

--- exercise-2.js
+++ exercise-2.js
@@ -1,10 +1,79 @@
-const pro = new Promise((res, rej) => {
-  const rand = Math.random();
-  if (rand > 0.5) {
-    return res("mmad")
-  } else {
-   throw rej("none mmad")
-   
-  }
-});
-pro.then(res => console.log(res)).catch(err => console.log(err))
+
+// let mmad;
+// console.log("first");
+// function orther() {
+// console.log("berfor settime");
+// setTimeout(() => {
+//   mmad = "call goli"
+// }, 2000);
+// console.log("after settime");
+// console.log(`mmad ${mmad}`);
+// }
+// orther()
+// console.log("after function");
+
+
+
+// let p = new Promise(function(resolve, reject) {
+//    setTimeout(function() {
+//       resolve();
+//    }, 3000);
+// });
+// p.then((res)=>{
+//   console.log(res);
+// })
+
+
+// let p = new Promise(function(resolve, reject) {
+//    resolve('First');
+// });
+
+// p.then(function(data) {
+//    console.log(data);
+// });
+
+// console.log('Second');
+
+
+// let p = new Promise(function(resolve) {
+//      resolve("OK");
+// });
+
+// let p2 = p.then(function(data) {
+//     return data;
+// });
+
+// let p3 = p2.then(function(data) {
+//     return data + " Bye";
+// });
+
+// p3.then((data)=>{
+//   console.log(data);
+// })
+
+
+// let p = new Promise(function (resolve, reject) {
+//   reject('Sorry');
+// })
+// .then((data) => console.log(data))
+// .catch((err) =>{});
+// console.log(p);
+
+
+// let p = new Promise(function(resolve, reject) {
+//     resolve("OK");
+// });
+
+// let p2 = p.then(function(data) {
+//     return data;
+// });
+
+// let p3 = p.then(function(data) {
+//     return data;
+// });
+
+// console.dir(p2 );
+// console.dir(p3 )
+// console.error(p2 === p3)
+
+ 
