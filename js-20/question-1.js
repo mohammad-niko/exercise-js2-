fb3b9beda439e7c0e14e405b57e6483ad8d17896
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-05-30 18:15:36 +0330

    js-20 Q

--- js-20/question-1.js
+++ js-20/question-1.js
@@ -3,3 +3,13 @@
 */
 
 // ! Answer
+
+// Definition: Synchronous functions execute one after the other in a sequence. The program waits for each operation to complete before moving on to the next one.
+
+// Blocking: They block the execution of other code until they are finished.
+
+
+
+// Definition: Asynchronous functions, on the other hand, do not block the execution of the program. They can start a task, continue executing other code, and then return to finish the task later, typically using mechanisms like callbacks, promises, or async/await.
+
+// Non-blocking: They allow the program to continue running while waiting for an operation (like a file read, HTTP request, etc.) to complete.
