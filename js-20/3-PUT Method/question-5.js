6556eac7346694a35c2dc7f42753a19a341031dc
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-05-30 15:27:02 +0330

    js-20 Q

--- js-20/3-PUT Method/question-5.js
+++ js-20/3-PUT Method/question-5.js
@@ -10,6 +10,57 @@
 //!PUT
 /** WRITE YOUR CODE BELOW DOWN */
 
-
-
 //*To be continue...
+
+async function replaceData(obj) {
+  const hed = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const response = await axios.put(
+      "https://683967476561b8d882b04142.mockapi.io/api/v1/users/4",
+      obj,
+      hed
+    );
+    const { data } = response;
+    alert("replace Data <3 ✅❤️");
+    console.log(data);
+  } catch (error) {
+    console.log(`ERORR FATCH: ${error}`);
+  }
+}
+
+const data = {
+  firstName: "Luffy",
+  lastName: "Monkey D.",
+  phoneNumber: "(945) 635-3854",
+};
+// replaceData(data);
+
+
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const first = form.elements["First"].value.trim();
+  const last = form.elements["Last"].value.trim();
+  const num = form.elements["num"].value.trim();
+
+  if (!first || !last || !num) {
+    return alert("Invalid value");
+  }
+
+  const obj = {
+    firstName: first,
+    lastName: last,
+    number: num,
+  };
+
+// replaceData(obj);
+
+  form.reset();
+});
+
